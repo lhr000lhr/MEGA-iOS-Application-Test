8e1493a719585c9f96ec68497cc64c8dce1cb9d4
lihaoran <8726682@qq.com>
2019-09-18 20:57:39 +1200

    feat(Model): create Realm model

---
+++ TWBudgetAndExpense/API/Models/MGCategory.h
@@ -0,0 +1,17 @@
+//
+//  MGCategory.h
+//  TWBudgetAndExpense
+//
+//  Created by haoran lee on 2019/9/18.
+//  Copyright © 2019 MEGA. All rights reserved.
+//
+
+#import "RLMObject.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MGCategory : RLMObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TWBudgetAndExpense/API/Models/MGCategory.m
@@ -0,0 +1,13 @@
+//
+//  MGCategory.m
+//  TWBudgetAndExpense
+//
+//  Created by haoran lee on 2019/9/18.
+//  Copyright © 2019 MEGA. All rights reserved.
+//
+
+#import "MGCategory.h"
+
+@implementation MGCategory
+
+@end
