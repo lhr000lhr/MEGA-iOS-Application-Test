6ca31616e98d83737acb1d8ca95a5968abd8c043
lihaoran <8726682@qq.com>
2019-09-20 11:37:14 +1200

    feat(MGAPIManager): create MGAPIManager

---
+++ TWBudgetAndExpense/API/MGAPIManager.h
@@ -0,0 +1,17 @@
+//
+//  MGAPIManager.h
+//  TWBudgetAndExpense
+//
+//  Created by haoran lee on 2019/9/20.
+//  Copyright © 2019 MEGA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MGAPIManager : AFHTTPRequestOperationManager
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TWBudgetAndExpense/API/MGAPIManager.m
@@ -0,0 +1,13 @@
+//
+//  MGAPIManager.m
+//  TWBudgetAndExpense
+//
+//  Created by haoran lee on 2019/9/20.
+//  Copyright © 2019 MEGA. All rights reserved.
+//
+
+#import "MGAPIManager.h"
+
+@implementation MGAPIManager
+
+@end
